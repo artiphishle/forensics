213a6f9814d77c52fe659dca792a9cbefdaefe23
artiphishle <fabio.gartenmann@protonmail.com>
2025-04-19 03:17:26 +0200

    Add test: 'filterSubPackages'

--- test/utils/filter/filterSubPackages.spec.ts
+++ test/utils/filter/filterSubPackages.spec.ts
@@ -1,9 +1,20 @@
+import { filterSubPackages } from '@/utils/filter/filterSubPackages';
+import { ElementsDefinition } from 'cytoscape';
 import assert from 'node:assert';
 import test, { describe } from 'node:test';
 
 describe('[filterSubPackages]', () => {
-  // Test:
-  test.skip('Sub packages are filtered correctly', () => {
-    assert.strictEqual(true, false);
+  // Test: Containing packages filtered corectly
+  test('Filters sub-packages correctly', () => {
+    const elements: ElementsDefinition = {
+      nodes: [{ data: { id: 'a.b' } }, { data: { id: 'a.b.c' } }, { data: { id: 'x.y.z' } }],
+      edges: [],
+    };
+    const filteredElements = filterSubPackages(elements);
+    const nodeIds = filteredElements.nodes.map(({ data }) => data.id);
+
+    assert.strictEqual(nodeIds.length, 2);
+    assert.strictEqual(nodeIds[0], 'a.b');
+    assert.strictEqual(nodeIds[1], 'x.y.z');
   });
 });
