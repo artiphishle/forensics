0dff5adbe40c50236a1d94d4fa8bd082dafe56ec
artiphishle <fabio.gartenmann@protonmail.com>
2025-04-19 04:00:55 +0200

    Add test: 'generateSequenceFromProject'

--- test/utils/mermaid/generateSequenceFromProject.spec.ts
+++ test/utils/mermaid/generateSequenceFromProject.spec.ts
@@ -1,9 +1,17 @@
+import { generateSequenceFromProject } from '@/utils/mermaid/generateSequenceFromProject';
 import assert from 'node:assert';
+import { resolve } from 'node:path';
 import test, { describe } from 'node:test';
 
 describe('[generateSequenceFromProject]', () => {
-  // Test:
-  test.skip('Correct sequence diagram is generated', () => {
-    assert.strictEqual(true, false);
+  // Test: Generates correct sequence diagram
+  test('Correct sequence diagram is generated', async () => {
+    process.env.NEXT_PUBLIC_PROJECT_PATH = resolve(process.cwd(), 'examples/java/my-app');
+    const sequence = await generateSequenceFromProject();
+
+    assert.strictEqual(
+      sequence,
+      'sequenceDiagram\nactor User\nUser->>App: main()\nApp->>out: println()'
+    );
   });
 });
